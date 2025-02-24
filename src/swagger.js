1fa58a98b1e336af3518e75416fdcfd548408f1e
Naomi <gabriella.naomi21@ui.ac.id>
2025-02-24 14:34:50 +0700

    add swagger

---
+++ src/swagger.js
@@ -0,0 +1,18 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+  info: {
+    title: 'API Testing',
+    description: 'Dokumentasi API dengan Swagger Autogen',
+  },
+  host: 'localhost:5000',
+  schemes: ['http'],
+};
+
+const outputFile = './src/swagger-output.json';
+const endpointsFiles = ['./src/server.js'];
+
+swaggerAutogen()(outputFile, endpointsFiles).then(() => {
+  // Jalankan server setelah dokumentasi tergenerate
+  import('./server');
+});
